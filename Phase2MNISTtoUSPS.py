66716ed493bf9e6b105dbf26030bbe91b258d0db
aabbas90 <ahmed.a.mirza@hotmail.com>
2017-07-27 03:10:11 +0200

    Update Phase2MNISTtoUSPS.py

--- Phase2MNISTtoUSPS.py
+++ Phase2MNISTtoUSPS.py
@@ -28,14 +28,15 @@ class LeNet(nn.Module):
         self.conv2 = nn.Conv2d(20, 50, 5)
         self.bn2 = nn.BatchNorm2d(50)
         self.fc1 = nn.Linear(50 * 4 * 4, 500)
-
+        self.bn3  = nn.BatchNorm1d(500)
+		
     def forward(self, x):
         out = F.leaky_relu(self.bn1(self.conv1(x)))
         out = F.max_pool2d(out, 2)
         out = F.leaky_relu(self.bn2(self.conv2(out)))
         out = F.max_pool2d(out, 2)
         out = out.view(out.size(0), -1)
-        out = F.leaky_relu(self.fc1(out))
+        out = F.leaky_relu(self.bn3(self.fc1(out)))
         return out
 
 class LeNetClassifier(nn.Module):
@@ -52,10 +53,12 @@ class Discriminator(nn.Module):
         self.fc1 = nn.Linear(500, numberHiddenUnitsD)
         self.fc2 = nn.Linear(numberHiddenUnitsD, numberHiddenUnitsD)
         self.fc3 = nn.Linear(numberHiddenUnitsD, 2)
-
+        self.bn1 = nn.BatchNorm1d(numberHiddenUnitsD)
+        self.bn2 = nn.BatchNorm1d(numberHiddenUnitsD)
+        
     def forward(self, x):
-        out = F.relu(self.fc1(x))
-        out = F.relu(self.fc2(out))
+        out = F.leaky_relu(self.bn1(self.fc1(x)))
+        out = F.leaky_relu(self.bn2(self.fc2(out)))
         return self.fc3(out)
 
 targetTrainDataset = USPSSAMPLE(root='./data/', num_training_samples=7438, train=True, transform=transforms.ToTensor())
